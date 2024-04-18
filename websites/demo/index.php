77c41119b8572c9fe7aff61df1261a29b37886e5
genie-ru <genie.zgmf987@gmail.com>
2024-04-19 08:16:44 +0900

    bodyに対してcssをかけ中央に文字を寄せる

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -4,14 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
-    <h1>
-        <?php
-            $greeting = "Hello";
-
-            echo $greeting . " " . "Everybody!";
-        ?>
+        
+            <h1>
+                You have read"Dark matter";
+            </h1>
+        
     </h1>
 </body>
 </html>
