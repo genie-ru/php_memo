40c4283e3fc6786eb012fe6e6c90ae6454253bfa
genie-ru <genie.zgmf987@gmail.com>
2024-04-21 05:51:47 +0900

    add array

---
+++ websites/001_array/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Recoomendations</h1>
+
+    <?php
+        $books = "Hail Mary";
+    ?>
+
+    <ul>
+        <li>Do Androids Dream of Electric Sheep?</li>
+        <li>The Langoliers</li>
+        <li>Hail Mary</li>
+</body>
+</html>
